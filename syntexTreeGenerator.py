24e1f8b5fdfe95093bc57e692672cdb588ba11cd
Empire <458580810@qq.com>
2017-07-04 20:37:36 +0800

    改造成了一个类

--- syntexTreeGenerator.py
+++ syntexTreeGenerator.py
@@ -3,100 +3,105 @@ import copy
 import json
 
 
-def str2Dict(string, p_list):
-    string = string.replace(' ', '')
-    m_list = string.split('->')
-    p_list.append((m_list[0], list(m_list[1])))
-    return p_list
+class ParsingTableGenerator(object):
+    """
+    class for generate predicting parsing table.
+    """
 
+    def __init__(self, grammar, terminators, non_terminators):
+        self.p_list = self.str2Dict(grammar)
+        self.terminators = terminators
+        self.non_terminators = non_terminators
+        self.first_dict = self.firstConstructor()
+        self.follow_dict = self.followConstructor()
 
-def firstConstructor(p_list, non_terminators):
-    first_dict = defaultdict(set)
-    for p in p_list:
-        nonT = p[0]
-        first_dict[nonT] = findFirst(nonT, p_list, non_terminators)
-    return first_dict
+    @classmethod
+    def str2Dict(self, grammar):
+        self.p_list = list()
+        for p in grammar:
+            p = p.replace(' ', '')
+            m_list = p.split('->')
+            self.p_list.append((m_list[0], list(m_list[1])))
+        return self.p_list
 
+    def firstConstructor(self):
+        first_dict = defaultdict(set)
+        for p in self.p_list:
+            nonT = p[0]
+            first_dict[nonT] = self.findFirst(nonT)
+        return first_dict
 
-def findFirst(symbol, p_list, non_terminators):
-    first_set = set()
-    if symbol not in non_terminators:
-        first_set.add(symbol)
-    else:
-        first_set |= set([p[1][0] for p in p_list if p[0] == symbol and p[1][0] not in non_terminators])
-        for p in p_list:
-            if p[0] == symbol and p[1][0] in non_terminators:
-                first_set |= findFirst(p[1][0], p_list, non_terminators) - set(('ε'))
-        first_set -= set([None])
-    return first_set
+    def findFirst(self, symbol):
+        first_set = set()
+        if symbol not in self.non_terminators:
+            first_set.add(symbol)
+        else:
+            first_set |= set([p[1][0] for p in self.p_list if p[0] == symbol and p[1][0] not in self.non_terminators])
+            for p in self.p_list:
+                if p[0] == symbol and p[1][0] in self.non_terminators:
+                    first_set |= self.findFirst(p[1][0]) - set(('ε'))
+            first_set -= set([None])
+        return first_set
 
+    def findFirstString(self, symbols):
+        first_set = set()
+        for symbol in symbols:
+            first_set |= self.findFirst(symbol) - set(('ε'))
+            if 'ε' not in self.findFirst(symbol):
+                break
+            elif symbol is symbols[-1]:
+                first_set.add('ε')
+        return first_set
 
-def findFirstString(symbols, p_list, non_terminators):
-    first_set = set()
-    for symbol in symbols:
-        first_set |= findFirst(symbol, p_list, non_terminators) - set(('ε'))
-        if 'ε' not in findFirst(symbol, p_list, non_terminators):
-            break
-        elif symbol is symbols[-1]:
-            first_set.add('ε')
-    return first_set
-
-
-def followConstructor(p_list, non_terminators):
-    follow_dict = defaultdict(set)
-    for p in p_list:
-        if p[0] is 'E':
-            follow_dict[p[0]].add('#')
-        for i, v in enumerate(p[1]):
-            if v in non_terminators and v is not p[1][-1]:
-                follow_dict[v] |= findFirst(p[1][i + 1], p_list, non_terminators) - set(('ε'))
-        
-    for p in p_list:
-        for i, v in enumerate(p[1]):
-            if v in non_terminators:
-                if v is p[1][-1]:
-                    follow_dict[v] |= follow_dict[p[0]]
-                else:
-                    if 'ε' in findFirstString(p[1][i + 1:], p_list, non_terminators):
+    def followConstructor(self):
+        follow_dict = defaultdict(set)
+        for p in self.p_list:
+            if p[0] is 'E':
+                follow_dict[p[0]].add('#')
+            for i, v in enumerate(p[1]):
+                if v in self.non_terminators and v is not p[1][-1]:
+                    follow_dict[v] |= self.findFirst(p[1][i + 1]) - set(('ε'))
+            
+        for p in self.p_list:
+            for i, v in enumerate(p[1]):
+                if v in self.non_terminators:
+                    if v is p[1][-1]:
                         follow_dict[v] |= follow_dict[p[0]]
-    return follow_dict
+                    else:
+                        if 'ε' in self.findFirstString(p[1][i + 1:]):
+                            follow_dict[v] |= follow_dict[p[0]]
+        return follow_dict
 
-
-def tableConstractor(follow_dict, p_list, terminators):
-    parsing_table = defaultdict(tuple)
-    for p in p_list:
-        if 'ε' in findFirstString(p[1], p_list, non_terminators):
-            for v in follow_dict[p[0]]:
-                parsing_table[(p[0], v)] = p
-            if '#' in follow_dict[p[0]]:
-                parsing_table[(p[0], '#')]
-        for v in terminators:
-            if v in findFirstString(p[1], p_list, non_terminators):
-                parsing_table[(p[0], v)] = p
-    return parsing_table
+    def tableConstractor(self):
+        self.parsing_table = defaultdict(tuple)
+        for p in self.p_list:
+            if 'ε' in self.findFirstString(p[1]):
+                for v in self.follow_dict[p[0]]:
+                    self.parsing_table[(p[0], v)] = p
+                if '#' in self.follow_dict[p[0]]:
+                    self.parsing_table[(p[0], '#')]
+            for v in terminators:
+                if v in self.findFirstString(p[1]):
+                    self.parsing_table[(p[0], v)] = p
+        return self.parsing_table
 
 
 if __name__ == '__main__':
-    p_list = []
-    rawP = ('E -> TR',
+    grammar = ('E -> TR',
             'R -> +TR',
             'R -> -TR',
             'R -> ε',
             'T -> (E)',
             'T -> i',
             'T -> n')
-    for p in rawP:
-        p_list = str2Dict(p, p_list)
 
     non_terminators = ('E', 'T', 'R')
     terminators = ('i', 'n', '(', ')', '+', '-')
-    print(p_list)
 
-    first_dict = firstConstructor(p_list, non_terminators)
-    print(firstConstructor(p_list, non_terminators))
+    parsingTableGenerator = ParsingTableGenerator(grammar, terminators, non_terminators)
 
-    follow_dict = followConstructor(p_list, non_terminators)
-    print(followConstructor(p_list, non_terminators))
-
-    parsing_table = tableConstractor(follow_dict, p_list, terminators)
+    parsing_table = parsingTableGenerator.tableConstractor()
+    print(parsingTableGenerator.p_list)
+    print(parsingTableGenerator.first_dict)
+    print(parsingTableGenerator.follow_dict)
     print(parsing_table)
